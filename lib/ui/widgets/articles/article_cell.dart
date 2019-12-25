1539180589d0b7b547916dcc195fd95ad9dad55e
Félix Simões <felixssimoes@gmail.com>
2019-12-25 14:48:22 +0100

    make article cell adaptive to vertical/horizontal layouts

--- lib/ui/widgets/articles/article_cell.dart
+++ lib/ui/widgets/articles/article_cell.dart
@@ -5,14 +5,21 @@ import 'package:newsapp/data/models/article.model.dart';
 const _cellBorderRadius = 5.0;
 const _imageHeigth = 105.0;
 
+enum ArticleCellLayout {
+  Vertical,
+  Horizontal,
+}
+
 class ArticleCell extends StatelessWidget {
   final Article article;
   final Function onPress;
+  final ArticleCellLayout layout;
 
   const ArticleCell({
     Key key,
     @required this.article,
     @required this.onPress,
+    this.layout = ArticleCellLayout.Vertical,
   })  : assert(article != null),
         assert(onPress != null),
         super(key: key);
@@ -33,27 +40,51 @@ class ArticleCell extends StatelessWidget {
                 offset: Offset(0, 5),
               )
             ]),
-        child: Column(
-          children: <Widget>[
-            _buildImage(),
-            _buildInfoSection(),
-          ],
-        ),
+        child: _buildContent(),
       ),
+    );
+  }
+
+  Widget _buildContent() {
+    if (layout == ArticleCellLayout.Vertical) {
+      return Column(
+        children: <Widget>[
+          _buildImage(),
+          _buildInfoSection(),
+        ],
+      );
+    }
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: <Widget>[
+        _buildImage(),
+        if (layout == ArticleCellLayout.Vertical) _buildInfoSection(),
+        if (layout == ArticleCellLayout.Horizontal)
+          Expanded(
+            child: _buildInfoSection(),
+          ),
+      ],
     );
   }
 
   Widget _buildImage() {
     if (article.imageUrl == null) {
-      return Placeholder(fallbackHeight: _imageHeigth);
+      return Placeholder(
+        fallbackHeight: _imageHeigth,
+        fallbackWidth: _imageHeigth,
+      );
     }
 
     return ClipRRect(
       borderRadius: new BorderRadius.circular(_cellBorderRadius),
       child: Image.network(
         article.imageUrl,
-        height: _imageHeigth,
-        width: double.infinity,
+        height: layout == ArticleCellLayout.Vertical
+            ? _imageHeigth
+            : double.infinity,
+        width: layout == ArticleCellLayout.Horizontal
+            ? _imageHeigth
+            : double.infinity,
         fit: BoxFit.cover,
       ),
     );
