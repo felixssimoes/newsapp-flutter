ffc7cff198b1a6cb07e4ffc410dfad6d21cf55be
Félix Simões <felixssimoes@gmail.com>
2019-12-25 16:21:44 +0100

    add pagination and pull to refresh on search screen

--- lib/ui/screens/search/search.screen.dart
+++ lib/ui/screens/search/search.screen.dart
@@ -3,11 +3,15 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:newsapp/app/navigator.dart';
 import 'package:newsapp/config/locator.config.dart';
+import 'package:newsapp/data/models/article.model.dart';
 import 'package:newsapp/data/providers/search.provider.dart';
+import 'package:newsapp/ui/widgets/adaptive_progress_indicator.dart';
+import 'package:newsapp/ui/widgets/adaptive_refreshable_sliver.dart';
 import 'package:newsapp/ui/widgets/articles/article_cell.dart';
 import 'package:provider/provider.dart';
 
 const _cellHeight = 140.0;
+const _padding = 6.0;
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -18,6 +22,7 @@ class _SearchScreenState extends State<SearchScreen> {
   final _navigator = locator<AppNavigator>();
   final _searchController = TextEditingController();
   Timer timeHandle;
+  bool _isLoadingMore = false;
 
   @override
   void initState() {
@@ -31,7 +36,7 @@ class _SearchScreenState extends State<SearchScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
-      body: _buildArticlesList(),
+      body: _buildContentList(),
     );
   }
 
@@ -83,25 +88,74 @@ class _SearchScreenState extends State<SearchScreen> {
     );
   }
 
-  Widget _buildArticlesList() {
+  Widget _buildContentList() {
     return Consumer<SearchProvider>(
-      builder: (BuildContext context, SearchProvider provider, Widget child) {
-        return ListView.builder(
-          itemCount: provider.articles.length,
-          itemExtent: _cellHeight,
-          itemBuilder: (_, index) {
-            final article = provider.articles[index];
-            return Padding(
-              padding: const EdgeInsets.only(bottom: 12, left: 12, right: 12),
-              child: ArticleCell(
-                article: article,
-                onPress: () => _navigator.openArticleScreen(article),
-                layout: ArticleCellLayout.Horizontal,
-              ),
-            );
+        builder: (BuildContext context, SearchProvider provider, Widget child) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: _padding),
+        child: NotificationListener<ScrollNotification>(
+          onNotification: (ScrollNotification scrollInfo) {
+            _onLoadMore(scrollInfo);
+            return false;
           },
-        );
-      },
+          child: AdaptiveRefreshableSliver(
+            onRefresh: () async => await _onPullToRefresh(),
+            slivers: <Widget>[
+              _buildArticlesSliver(provider),
+              _buildLoadMoreSliver(provider),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+
+  Widget _buildArticlesSliver(SearchProvider provider) {
+    final size = MediaQuery.of(context).size;
+    final cellWidth = (size.width - _padding * 3);
+    final childAspectRatio = cellWidth / _cellHeight;
+
+    return SliverGrid(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 1,
+        childAspectRatio: childAspectRatio,
+      ),
+      delegate: SliverChildBuilderDelegate(
+        (BuildContext context, int index) {
+          final article = provider.articles[index];
+          return Padding(
+            padding: const EdgeInsets.only(
+              left: _padding,
+              right: _padding,
+              top: _padding * 2,
+            ),
+            child: ArticleCell(
+              article: article,
+              layout: ArticleCellLayout.Horizontal,
+              onPress: () => _onPressArticle(article),
+            ),
+          );
+        },
+        childCount: provider.articles.length,
+      ),
+    );
+  }
+
+  Widget _buildLoadMoreSliver(SearchProvider provider) {
+    return SliverToBoxAdapter(
+      child: SafeArea(
+        child: provider.canLoadMore
+            ? Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: _padding * 4,
+                    bottom: _padding * 2,
+                  ),
+                  child: AdaptiveProgressIndicator(),
+                ),
+              )
+            : Container(height: _padding * 2),
+      ),
     );
   }
 
@@ -114,9 +168,31 @@ class _SearchScreenState extends State<SearchScreen> {
     });
   }
 
+  Future<void> _onPullToRefresh() async {
+    await Provider.of<SearchProvider>(context, listen: false)
+        .loadSearchNews(text: _searchController.text);
+  }
+
   Future<void> _doSearch(String value) async {
     print("Calling now the API: $value");
     final provider = Provider.of<SearchProvider>(context, listen: false);
     await provider.loadSearchNews(text: value);
+  }
+
+  void _onLoadMore(
+    ScrollNotification scrollInfo,
+  ) async {
+    final provider = Provider.of<SearchProvider>(context, listen: false);
+    if (!_isLoadingMore &&
+        provider.canLoadMore &&
+        scrollInfo.metrics.pixels >= scrollInfo.metrics.maxScrollExtent * 0.8) {
+      _isLoadingMore = true;
+      await provider.loadSearchNewsNextPage();
+      _isLoadingMore = false;
+    }
+  }
+
+  void _onPressArticle(Article article) {
+    _navigator.openArticleScreen(article);
   }
 }
