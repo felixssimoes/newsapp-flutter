0232ddffa5955f4e450cab08afdb41eac94cd0c8
Felix Simoes <felix.simoes@njiuko.com>
2024-03-18 15:54:26 +0100

    Remove unused import statement

--- lib/sources/application/sources.service.dart
+++ lib/sources/application/sources.service.dart
@@ -2,8 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:newsapp/articles/articles.dart';
 import 'package:newsapp/sources/sources.dart';
 
-import '../data/sources.repository.dart';
-
 class SourcesService {
   SourcesService(this._ref);
   final Ref _ref;
